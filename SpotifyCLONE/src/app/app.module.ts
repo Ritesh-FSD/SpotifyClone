38ebcc9dc9a3d5aa709ec3516afcba9cc7038c46
Kumar <ritesh.ca.kumar@accenture.com>
2024-06-13 17:05:43 +0530

    created login signup home player playlist signup component

--- SpotifyCLONE/src/app/app.module.ts
+++ SpotifyCLONE/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PlaylistComponent } from './playlist/playlist.component';
+import { SignupComponent } from './signup/signup.component';
+import { PlayerComponent } from './player/player.component';
+import { FormsModule } from '@angular/forms';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    PlaylistComponent,
+    SignupComponent,
+    PlayerComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ SpotifyCLONE/src/app/signup/signup.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+  email:any='';
+  password:any='';
+  confirmPassword:any='';
+
+  signup(){
+    
+  }
+}
